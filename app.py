db5ba2981d3d7102d137515b54d94d162b9b3cfd
David Kamande <DKamande@CO-OPBANK.CO.KE>
2025-09-28 13:57:56 +0300

    Update Mail configs

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'dangawalla@gmail.com' 
-app.config['MAIL_PASSWORD'] = 'hkni pfez suuu mmjz'
+app.config['MAIL_PASSWORD'] = 'wehu dunc xfys aknp'
 app.config['MAIL_DEFAULT_SENDER'] = 'noreply@demo.com'
 
 
